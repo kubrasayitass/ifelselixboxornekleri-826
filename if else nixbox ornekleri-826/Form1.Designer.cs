ea72c3d45ba02a019b6948dcd9ed88f63e31411d
kubrasayitass <145451189+kubrasayitass@users.noreply.github.com>
2023-10-10 16:33:07 +0300

    Add files via upload

---
+++ if else nixbox ornekleri-826/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace if_else_nixbox_ornekleri_826
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTeksayi = new System.Windows.Forms.Label();
+            this.lblCiftsayi = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.txtSayi = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblTeksayi
+            // 
+            this.lblTeksayi.AutoSize = true;
+            this.lblTeksayi.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTeksayi.Location = new System.Drawing.Point(28, 93);
+            this.lblTeksayi.Name = "lblTeksayi";
+            this.lblTeksayi.Size = new System.Drawing.Size(71, 24);
+            this.lblTeksayi.TabIndex = 0;
+            this.lblTeksayi.Text = "tek sayı";
+            // 
+            // lblCiftsayi
+            // 
+            this.lblCiftsayi.AutoSize = true;
+            this.lblCiftsayi.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCiftsayi.Location = new System.Drawing.Point(198, 93);
+            this.lblCiftsayi.Name = "lblCiftsayi";
+            this.lblCiftsayi.Size = new System.Drawing.Size(69, 24);
+            this.lblCiftsayi.TabIndex = 1;
+            this.lblCiftsayi.Text = "çift sayı";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 123);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(123, 199);
+            this.listBox1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(167, 123);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(119, 199);
+            this.listBox2.TabIndex = 3;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(155, 23);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(102, 42);
+            this.btnEkle.TabIndex = 4;
+            this.btnEkle.Text = "ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // txtSayi
+            // 
+            this.txtSayi.Location = new System.Drawing.Point(12, 33);
+            this.txtSayi.Name = "txtSayi";
+            this.txtSayi.Size = new System.Drawing.Size(111, 20);
+            this.txtSayi.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtSayi);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lblCiftsayi);
+            this.Controls.Add(this.lblTeksayi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTeksayi;
+        private System.Windows.Forms.Label lblCiftsayi;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.TextBox txtSayi;
+    }
+}
+
---
+++ if else nixbox ornekleri-826/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace if_else_nixbox_ornekleri_826
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt32(txtSayi.Text);
+            if (sayi % 2 == 0)
+            {
+                listBox1.Items.Add(txtSayi.Text);
+            }
+            else
+            {
+                listBox2.Items.Add(txtSayi.Text);
+                    
+            }
+        }
+    }
+}
